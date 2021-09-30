5672025d380d44a4edcbd09319230005f3ab6ebb
jomom <momanyi330@gmail.com>
2021-09-30 15:59:21 +0300

    frontend set up

---
+++ config/database.js
@@ -0,0 +1,4 @@
+ module.exports ={
+ 	database: 'mongodb+srv://rootuser:keyboardrat@cluster0.zhbtm.mongodb.net/vibradb?retryWrites=true&w=majority',
+   // database:'mongodb://localhost:27017/storedb' ,
+ }
